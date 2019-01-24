fdb60deb953fa28b9fecf4b255eea2d27b8a9441
Matthew Ingersoll <matth@mtingers.com>
2019-01-23 20:15:12 -0800

    Version v0.1.2

--- opacify/opacifyinfo.py
+++ opacify/opacifyinfo.py
@@ -1,5 +1,5 @@
 
 AUTHOR  = 'Matth Ingersoll <matth@mtingers.com>'
-COMMIT  = ''
+COMMIT  = '269305e0ffecff94d488c4cba870fccdc88959d3'
 VERSION = 'v0.1.2'
 PROJECT = 'http://github.com/mtingers/opacify'
