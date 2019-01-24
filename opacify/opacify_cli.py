5f01301dc6a6340d8f10fac077461dfa3fa196d5
Matthew Ingersoll <matth@mtingers.com>
2019-01-23 20:31:52 -0800

    Add CLI

---
+++ opacify/opacify_cli.py
@@ -0,0 +1,87 @@
+import sys
+import os
+import time
+import argparse
+from opacify import Opacify
+from opacify import INFOTXT, EPILOG
+
+def version():
+    return INFOTXT
+
+#if __name__ == '__main__':
+def main():
+    parser = argparse.ArgumentParser(
+        description=INFOTXT,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog=EPILOG
+    )
+    subparser = parser.add_subparsers(dest='func')
+    if sys.version_info[0] < 3:
+        subparser.required = True     
+    group1 = subparser.add_parser('pacify', description='Run in pacify mode (builds manifest from input file)',
+         help='Run in pacify mode (builds manifest from input file)')
+    group2 = subparser.add_parser('depacify', description='Run in depacify mode (rebuilds file using manifest)',
+        help='Run in depacify mode (extracts file using manifest)')
+    group3 = subparser.add_parser('verify', description='Validate manifest URLs and response length',
+        help='Validate manifest URLs and response length')
+    # Pacify
+    group1.add_argument('-i', '--input', required=True, help='Path to input file')
+    group1.add_argument('-u', '--urls', required=True, help='Path to urls file')
+    group1.add_argument('-m', '--manifest', required=True, help='Output path of manifest file')
+    group1.add_argument('-c', '--cache', required=True, help='Path to cache directory')
+    group1.add_argument('-k', '--keep', action='store_const', const=True,
+        help='Do not remove cache after completed. Useful for testing')
+    group1.add_argument('-f', '--force', action='store_const', const=True, help='Overwrite manifest if it exists')
+    group1.add_argument('-d', '--debug', action='store_const', const=True, help='Turn on debug output')
+    # Depacify
+    group2.add_argument('-m', '--manifest', required=True, help='Path of manifest file')
+    group2.add_argument('-o', '--out', required=True, help='Path to write output file to')
+    group2.add_argument('-c', '--cache', required=True, help='Path to cache directory')
+    group2.add_argument('-k', '--keep', action='store_const', const=True,
+        help='Do not remove cache after completed. Useful for testing')
+    group2.add_argument('-f', '--force', action='store_const', const=True, help='Overwrite output file if it exists', default=False)
+    group2.add_argument('-d', '--debug', action='store_const', const=True, help='Turn on debug output')
+    group3.add_argument('-m', '--manifest', required=True, help='Path of manifest file')
+    group3.add_argument('-d', '--debug', action='store_const', const=True, help='Turn on debug output')
+    parser.add_argument('-V', '--version', help='Display Opacify version info',
+        action='version', version=version()) #'%(prog)s '+VERSION)
+    args = parser.parse_args()
+    cache = 'cache'
+    if args.func in ('pacify', 'depacify'):
+        if args.cache:
+            cache = args.cache
+    start_timer = time.time()
+    debug = getattr(args, 'debug', False)
+    o = Opacify(cache_dir=cache, debug=debug)
+    if args.func == 'pacify':
+        r = o.pacify(
+            input_file=args.input,
+            url_file=args.urls,
+            manifest=args.manifest,
+            overwrite=args.force,
+            keep_cache=args.keep
+        )
+        end_timer = time.time()
+        print('Wrote manifest to: %s' % (args.manifest))
+        print('    Manifest size: %s' % (os.path.getsize(args.manifest)))
+        print('    Original size: %s' % (r[1]))
+        print('     Input sha256: %s' % (r[0]))
+        print('         Duration: %.3fs' % (end_timer - start_timer))
+    elif args.func == 'depacify':
+        r = o.depacify(manifest=args.manifest, out_file=args.out, keep_cache=args.keep, overwrite=args.force)
+        end_timer = time.time()
+        print('    Manifest size: %s' % (os.path.getsize(args.manifest)))
+        print('    Output sha256: %s' % (r[0]))
+        print('      Output size: %s' % (r[1]))
+        print('         Duration: %.3fs' % (end_timer - start_timer))
+    else:
+        parser.print_help()
+        if args.func:
+            if args.func != 'func':
+                print("")
+                print('NOTICE: Not yet implemented: %s' % (args.func))
+                print("")
+    msgs = o.messages()
+    for m in msgs:
+        print('%s: %s' % (m[0], m[1]))
+
--- setup.py
+++ setup.py
@@ -3,16 +3,19 @@ import setuptools
 
 setup(
     name='Opacify',
-    version='0.1.2',
+    version='0.1.3',
     author='Matth Ingersoll',
     author_email='matth@mtingers.com',
     packages=['opacify',],
     license='BSD 2-Clause License',
     long_description=open('README.md').read(),
     url='https://github.com/mtingers/opacify',
-    scripts=['bin/opacify',],
     install_requires=[
         "requests",
     ],
+    #scripts=['bin/opacify',],
+    entry_points={
+        'console_scripts': ['opacify=opacify.opacify_cli:main',],
+    },
 )
 
