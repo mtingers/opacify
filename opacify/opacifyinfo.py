784cd2f40804ecdd6a85989933ed69d955e3f360
Matthew Ingersoll <matth@mtingers.com>
2019-01-23 19:02:46 -0800

    Tag and bump version

--- opacify/opacifyinfo.py
+++ opacify/opacifyinfo.py
@@ -1,5 +1,5 @@
 
 AUTHOR  = 'Matth Ingersoll <matth@mtingers.com>'
-COMMIT  = '25dfdb32e1f349478fae42f0496c27c352cfd6da'
+COMMIT  = '754ed2be468c3b626d827c0cbfec3d3bcdc30dd0'
 VERSION = 'v0.1.1'
 PROJECT = 'http://github.com/mtingers/opacify'
